0d460cd471f76767f08c4704843243d56c736797
useraxadov2-ai <useraxadov2@gmail.com>
2026-09-09 11:39:39 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -289,7 +289,7 @@ def replace_emojis_in_text(text: str) -> str:
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8746996595:AAEIbidc49taYa-CVHhWLkYWU_gv77xESpQ")
 FIREBASE_DB_URL = os.getenv("FIREBASE_DB_URL", "https://injoygame-cba8a-default-rtdb.firebaseio.com/").rstrip("/")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AQ.Ab8RN6JOYd6DcNYIYECcvWG7azASP4pmOMEoeuG9ttR-T0KC4A")
-FIREBASE_AUTH = os.getenv("FIREBASE_AUTH", "AQ.Ab8RN6JOYd6DcNYIYECcvWG7azASP4pmOMEoeuG9ttR-T0KC4A")
+FIREBASE_AUTH = os.getenv("FIREBASE_AUTH", "4dtBCR5DJbHA0TIODthmpaIM7Ieta9quuZxmQUB3")
 
 AI_MODEL = "gemini-3-flash-preview"
 
